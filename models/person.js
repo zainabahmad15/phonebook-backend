767ff7ce29b1236fed0a5c1a909e069054bd56bd
zainabahmad <zainaa1121@gmail.com>
2023-05-11 17:44:32 +0300

    converted uri to string for mongodb connection

--- models/person.js
+++ models/person.js
@@ -7,7 +7,7 @@ const url = process.env.MONGODB_URI
 
 console.log('connecting to', url)
 
-mongoose.connect(url)
+mongoose.connect(url.toString)
     .then(result => {
         console.log('connected to MongoDB')
     })
