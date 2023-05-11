f9e3a2431e5e7ef9742ec3a5aec6344af650c3dc
zainabahmad <zainaa1121@gmail.com>
2023-05-12 00:13:16 +0300

    Fixed render environment variables

--- models/person.js
+++ models/person.js
@@ -5,7 +5,7 @@ mongoose.set('strictQuery', false)
 
 const url = process.env.MONGODB_URI
 
-console.log('connecting to', url)
+//console.log('connecting to', url)
 
 mongoose.connect(url)
     .then(result => {
